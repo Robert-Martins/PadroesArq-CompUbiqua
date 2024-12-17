b3cda305cd0bfc8a2f726600e65b05bfd5fb3788
Robert-Martins <robertcantares@gmail.com>
2024-12-17 08:14:56 -0300

    feat/icon-component-with-material-icons

---
+++ agende-saude-app/core/components/atoms/icons/Icon.tsx
@@ -0,0 +1,9 @@
+import { IconProps } from "@/core/vo/consts/components.props"
+import { MaterialCommunityIcons } from "@expo/vector-icons"
+import React from "react"
+
+const Icon: React.FC<IconProps> = (props) => {
+    return <MaterialCommunityIcons name={props.name} size={props.size ?? 32} color={props.color} />
+}
+
+export default Icon;
