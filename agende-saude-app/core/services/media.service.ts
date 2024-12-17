739e2029c06964082eb23cd4c626fe93acc373bf
Robert-Martins <robertcantares@gmail.com>
2024-12-17 17:04:30 -0300

    feat/update-media-service-approach-and-include-update

--- agende-saude-app/core/services/media.service.ts
+++ agende-saude-app/core/services/media.service.ts
@@ -3,9 +3,24 @@ import { Media } from "../models/media.model"
 
 const MEDIA_PATH: string = 'media'
 
-export const createMedia = (media: Media): Promise<Media> => {
-    return api.post(MEDIA_PATH, media)
-        .then(response => response.data);
+export const createMedia = (file: File): Promise<Media> => {
+    const formData: FormData = new FormData();
+    formData.append('file', file);
+    return api.post<Media>(MEDIA_PATH, formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    }).then(response => response.data);
+}
+
+export const updateMedia = (id: number, file: File): Promise<Media> => {
+    const formData: FormData = new FormData();
+    formData.append('file', file);
+    return api.put<Media>(`${MEDIA_PATH}/${id}`, formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    }).then(response => response.data);
 }
 
 export const deleteMedia = (id: number): Promise<void> => {
