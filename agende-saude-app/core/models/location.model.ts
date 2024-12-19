ed00d02a52332af347de6c4a015610c1eecedb13
Robert-Martins <robertcantares@gmail.com>
2024-12-19 02:25:02 -0300

    feat/update-location

--- agende-saude-app/core/models/location.model.ts
+++ agende-saude-app/core/models/location.model.ts
@@ -6,7 +6,7 @@ export class Location {
     constructor(
         public id: number = null,
         public name: string = null,
-        public accessEmergencies: boolean = null,
+        public acceptsEmergencies: boolean = null,
         public distance: number = null,
         public user: User = null,
         public thumbnail: Media = null,
