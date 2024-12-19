88abc9ed332fbe4cf6cb92a8cced24cfce1e07d4
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2024-12-18 23:45:26 -0300

    fix/location-findAll

--- agende-saude_api/src/main/java/br/com/agendesaude/api/controller/LocationController.java
+++ agende-saude_api/src/main/java/br/com/agendesaude/api/controller/LocationController.java
@@ -6,9 +6,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,11 +34,9 @@ public class LocationController {
 
   @GetMapping
   public ResponseEntity<Page<LocationDto>> findAllLocations(
-      @RequestParam(required = false) String name,
-      @RequestParam(required = false) Boolean acceptsEmergencies,
-      Pageable pageable) {
+          @PageableDefault(size = 10, sort = "name", direction = Sort.Direction.ASC) Pageable pageable) {
 
-    Page<LocationDto> result = locationService.findAllLocations(name, acceptsEmergencies, pageable);
+    Page<LocationDto> result = locationService.findAllLocations(pageable);
     return ResponseEntity.ok(result);
   }
 
