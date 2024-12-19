ac7cb52ad5c3e36a9daae47a9d1e3fe314780d47
Robert-Martins <robertcantares@gmail.com>
2024-12-19 01:21:28 -0300

    feat/app-application-health-request

--- agende-saude-app/core/services/application.service.ts
+++ agende-saude-app/core/services/application.service.ts
@@ -4,6 +4,11 @@ import api from "../configs/axios/http-common";
 
 const APPLICATION_PATH: string = 'application';
 
+export const applicationHealthCheck = (): Promise<string> => {
+    return api.get<string>(`${APPLICATION_PATH}/health`)
+        .then(response => response.data);
+}
+
 export const findEnumByName = (name: AgendeSaudeEnumsNames): Promise<Enum> => {
     return api.get<Enum>(`${APPLICATION_PATH}/enum-by-name/${name}`)
         .then(response => response.data);
