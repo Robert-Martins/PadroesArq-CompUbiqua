290fa9decda010c269c4bbce704fee51470796fb
Yuan <yuancalixto@gmail.com>
2024-12-19 14:54:20 -0300

    feat/screening-controller

--- agende-saude_api/src/main/java/br/com/agendesaude/api/controller/ScreeningController.java
+++ agende-saude_api/src/main/java/br/com/agendesaude/api/controller/ScreeningController.java
@@ -4,6 +4,7 @@ import br.com.agendesaude.api.domain.service.ScreeningService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,17 +21,10 @@ public class ScreeningController {
     return ResponseEntity.ok(questions);
   }
 
-//  @GetMapping("/{screeningId}/answered")
-//  public ResponseEntity<List<ScreeningQuestionnaireAnswerDto>> getAnsweredScreening(@PathVariable Long screeningId) {
-//    List<ScreeningQuestionnaireAnswerDto> answers = screeningService.getAnsweredScreening(screeningId);
-//    return ResponseEntity.ok(answers);
-//  }
-//
-//  @PostMapping("/answers")
-//  public ResponseEntity<Screening> sendScreeningAnswers(@RequestBody ScreeningDto screeningDto) {
-//    Screening savedScreening = screeningService.saveScreening(screeningDto);
-//    return ResponseEntity.ok(savedScreening);
-//  }
-
+  @GetMapping("/{screeningId}/answered")
+  public ResponseEntity<Object> getAnsweredScreening(@PathVariable Long screeningId) {
+    Object answers = screeningService.getAnsweredScreening(screeningId);
+    return ResponseEntity.ok(answers);
+  }
 
 }
