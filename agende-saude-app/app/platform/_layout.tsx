f134ebb489e8d43d91e1bcca4024458620910c10
Robert-Martins <robertcantares@gmail.com>
2024-12-19 02:53:40 -0300

    feat/customize-tab-groups-layouts

--- agende-saude-app/app/platform/_layout.tsx
+++ agende-saude-app/app/platform/_layout.tsx
@@ -1,20 +1,33 @@
+import { Icon } from "@/core/components";
 import { Tabs } from "expo-router";
 
 const PlatformLayout: React.FC = () => {
     return (
-      <Tabs>
-            <Tabs.Screen name="home" options={{ title: "Home", tabBarIcon: () => null }} />
+      <Tabs 
+        screenOptions={{ 
+            headerShown: false,
+            tabBarStyle: {
+                backgroundColor: '#f8f9fa',
+                height: 56,
+                borderTopWidth: 0,
+                paddingBottom: 0,
+            },
+            tabBarActiveTintColor: '#007bff',
+            tabBarInactiveTintColor: '#6c757d',
+        }}
+      >
+            <Tabs.Screen name="home" options={{ title: "Home", tabBarIcon: ({ color, size }) => <Icon name="home-outline" color={color} size={size} /> }} />
             <Tabs.Screen
                 name="consultation"
-                options={{ title: "Consultas", tabBarIcon: () => null }}
+                options={{ title: "Consultas", tabBarIcon: ({ color, size }) => <Icon name="clipboard-edit-outline" color={color} size={size} /> }}
             />
             <Tabs.Screen
                 name="location"
-                options={{ title: "Locais", tabBarIcon: () => null }}
+                options={{ title: "Locais", tabBarIcon: ({ color, size }) => <Icon name="hospital-building" color={color} size={size} /> }}
             />
             <Tabs.Screen
                 name="profile"
-                options={{ title: "Perfil", headerShown: false }} 
+                options={{ title: "Perfil", tabBarIcon: ({ color, size }) => <Icon name="account-check-outline" color={color} size={size} /> }}
             />
       </Tabs>
     );
--- agende-saude-app/app/platform/consultation/_layout.tsx
+++ agende-saude-app/app/platform/consultation/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 
 const ConsultationLayout: React.FC = () => {
     return (
-        <Stack>
+        <Stack screenOptions={{ headerShown: false }}>
             <Stack.Screen name="index" options={{ title: "Consultas" }} />
             <Stack.Screen name="[id]" options={{ title: "Detalhes da Consulta" }} />
             <Stack.Screen name="appoint" options={{ title: "Nova Consulta" }} />
