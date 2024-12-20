9cb5676170c2e94fed0e3ecd914127a771492967
Robert-Martins <robertcantares@gmail.com>
2024-12-20 18:27:16 -0300

    feat/consultations-list-page

--- agende-saude-app/core/services/consultation.service.ts
+++ agende-saude-app/core/services/consultation.service.ts
@@ -3,6 +3,7 @@ import { Consultation } from "../models/consultation.model";
 import { Page } from "../vo/types/types";
 import { ConsultationFilter, PageableFilter } from "../vo/types/filters";
 import { createRequestParams } from "../utils/utils";
+import { Optional } from "../utils/optional";
 
 const CONSULTATION_PATH: string = 'consultation';
 
@@ -16,7 +17,9 @@ export const findConsultationById = (id: number): Promise<Consultation> => {
         .then(response => response.data);
 }
 
-export const findAllCommonConsultationsByLocationId = (locationId: number): Promise<Page<Consultation>> => {
-    return api.get<Page<Consultation>>(`${CONSULTATION_PATH}/location/${locationId}`)
-        .then(response => response.data);
+export const findAllCommonConsultationsByLocationId = (locationId: number, page: number, size: number): Promise<Page<Consultation>> => {
+    return api.get<Page<Consultation>>(`${CONSULTATION_PATH}/location/${locationId}`, { params:{
+        page: Optional.ofNullable(page).map(p => p.toString()).orElse('1'),
+        size: Optional.ofNullable(size).map(s => s.toString()).orElse('10'),
+    }}).then(response => response.data);
 }
